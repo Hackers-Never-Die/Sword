9e481ac82bbbf7c994cc8aa0ba322366a999b309
Jensen <jensenczx@163.com>
2020-05-15 22:56:10 +0800

    accountbookfragment

--- app/src/main/java/com/candy/caishenaccountbook/home/ui/accountbook/AccountBookFragment.kt
+++ app/src/main/java/com/candy/caishenaccountbook/home/ui/accountbook/AccountBookFragment.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.candy.caishenaccountbook.R
 import com.candy.caishenaccountbook.base.BaseActivity
 
 class AccountBookFragment : Fragment() {
 
+    private var mTotalNumber : TextView? = null
+    private var mIncomeNumber : TextView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         (activity as BaseActivity).changeStatusBarColor(R.color.themeColor)
-        return  inflater.inflate(R.layout.fragment_accountbook, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_accountbook, container, false)
+        initView(view)
+        return  view
+    }
+
+    private fun initView(view : View) {
+        mTotalNumber = view.findViewById(R.id.total_asset_number)
+        mIncomeNumber = view.findViewById(R.id.today_income_number)
+        mTotalNumber?.setOnClickListener {  }
     }
 }
